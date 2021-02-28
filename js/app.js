67a2a5bdfab14ab934e2a55f45c89c33fea271e1
Sam Atkinson <sammodeste@yahoo.com>
2021-02-28 00:43:01 +0000

    JavaScript added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const main = document.getElementsByTagName("main");
+
+
+
+document.querySelector('[data-switch-dark]').addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+  });
